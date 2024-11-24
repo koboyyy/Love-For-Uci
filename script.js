d68237967a9beee67671005d506ce70173003a08
koboyyy <ekoo200619@gmail.com>
2024-11-24 09:04:33 +0700

    play botton

--- script.js
+++ script.js
@@ -39,3 +39,13 @@ function createHeart() {
 
 // Tambahkan hati setiap 100ms agar terlihat lebih banyak
 setInterval(createHeart, 100); // Lebih sering menambahkan hati
+
+const audioPlayer = document.getElementById("audio-player");
+const playButton = document.getElementById("play-button");
+
+playButton.addEventListener("click", () => {
+  if (audioPlayer.paused) {
+    audioPlayer.play();
+    playButton.style.pointerEvents = "none"; // Nonaktifkan interaksi setelah musik mulai
+  }
+});
